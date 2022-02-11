a8e7439387fb22ed99fba793f44507cb40f2830e
Godson <excanny@yahoo.com>
2022-02-11 15:24:44 +0100

    Add project files.

---
+++ CoureAlgoEx/CheckInt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoureAlgoEx
+{
+    public class CheckInt
+    {
+        public static int CheckIntVal(List<int> input)
+        {
+            var count = 0;
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] % 2 == 0)
+                    count++;
+                if (input[i] % 2 != 0)
+                    count += 3;
+                if (input[i] == 8)
+                    count += 5;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ CoureAlgoEx/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoureAlgoEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new List<int>() { 1, 5, 25, 35};
+
+            Console.WriteLine(CheckInt.CheckIntVal(input));
+        }
+    }
+}
